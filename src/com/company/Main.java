451042625401cf16eb639fd58f86b982872288fe
Sam <sam12boy@sina.com>
2022-01-16 17:22:03 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+	    System.out.println(Fibonacci(6));
+    }
+    public static int Fibonacci(int n){
+        if (n>2){
+            return Fibonacci(n-1)+Fibonacci(n-2);
+        }else{
+            return n;
+        }
+    }
+}
